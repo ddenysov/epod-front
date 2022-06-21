3552b4ea0d414e6130deab4550eecf652dcc4d93
Dmytro Denysov <dmytro.denysov@danads.se>
2022-06-21 12:32:21 +0300

    Update deploy commend

---
+++ components/layout/FooterBlock.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="footer mt-auto">
+    <div class="footer-bottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="footer-copyright-text">
+              <p class="mb-0">© 2022, <strong>Podiya</strong>. All rights reserved. Powered by Denysov D.V.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  /**
+   * Component name
+   */
+  name: 'FooterBlock'
+}
+</script>
+
