51a5634bdf1929aa0d71b0520dbe132aa05dd1e6
Dmytro Denysov <dmytro.denysov@danads.se>
2022-07-09 00:05:12 +0300

    Initial commit

--- components/ui/steps/mixins/StepMixin.js
+++ components/ui/steps/mixins/StepMixin.js
@@ -1,19 +1,41 @@
 export default {
+  /**
+   * Inject properties
+   */
   inject: ['steps', 'currentStep', 'active', 'next', 'active', 'back'],
 
+  /**
+   * Computed properties
+   */
   computed: {
+    /**
+     * Is back button enabled
+     * @returns {boolean}
+     */
     backEnabled () {
       return this.active() > 0;
     },
 
+    /**
+     * Is next button enabled
+     * @returns {boolean}
+     */
     nextEnabled () {
       return this.active() < this.steps.length - 1;
     },
 
+    /**
+     * Is submit button enabled
+     * @returns {boolean}
+     */
     submitEnabled () {
       return this.active() === this.steps.length - 1;
     },
 
+    /**
+     * Get current step props
+     * @returns {*}
+     */
     currentStep () {
       return this.steps[this.active()];
     }
